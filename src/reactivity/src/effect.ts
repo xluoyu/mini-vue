e9ee1e964d301dd293248f2d324fc81d194e46b7
萧萧落雨 <8071489+xluoyu@user.noreply.gitee.com>
2021-08-16 09:59:44 +0800

    u

--- src/reactivity/src/effect.ts
+++ src/reactivity/src/effect.ts
@@ -55,6 +55,7 @@ export function effect(fn, options = {}) {
 export function stop(runner) {
   runner.effect.stop();
 }
+// hhh
 
 export function track(target, type, key) {
   // 1. 先基于 target 找到对应的 dep
